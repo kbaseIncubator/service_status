d678b9f9ff1bc4742c85b2c84404f47867b9f4e7
Bill Riehl <wjriehl@lbl.gov>
2019-11-04 18:36:27 -0800

    Updates

---
+++ precache-manifest.8c3ef7fddf19278a1ffc11c54a7acba1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34a2d842d0b0e1cd294ac59d67f4df40",
+    "url": "/service_status/index.html"
+  },
+  {
+    "revision": "9091ac7a5c14f64625c8",
+    "url": "/service_status/static/css/main.ec785282.chunk.css"
+  },
+  {
+    "revision": "ace8085f1d1f4b2210c7",
+    "url": "/service_status/static/js/2.2a4b3552.chunk.js"
+  },
+  {
+    "revision": "9091ac7a5c14f64625c8",
+    "url": "/service_status/static/js/main.b450ce85.chunk.js"
+  },
+  {
+    "revision": "2685c89f8cb0b2b00414",
+    "url": "/service_status/static/js/runtime-main.528dac54.js"
+  }
+]);
